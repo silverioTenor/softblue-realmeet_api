029b64ce6890313632d95b61a352c852f0de11d0
Rodrigo Silvério <silverio_tenor@outlook.com>
2023-11-27 18:59:37 -0300

    First commit

---
+++ service/src/test/java/br/com/sw2you/realmeet/ApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.sw2you.realmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
