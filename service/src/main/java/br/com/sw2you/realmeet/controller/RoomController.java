1843384a3a2e956e74bfe39c308f8038780ea88d
Rodrigo Silvério <silverio_tenor@outlook.com>
2024-01-28 18:37:47 -0300

    WIP

---
+++ service/src/main/java/br/com/sw2you/realmeet/controller/RoomController.java
@@ -0,0 +1,28 @@
+package br.com.sw2you.realmeet.controller;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
+import br.com.sw2you.realmeet.api.facade.RoomsApi;
+import br.com.sw2you.realmeet.api.model.RoomDTO;
+import br.com.sw2you.realmeet.service.RoomService;
+import br.com.sw2you.realmeet.util.ResponseEntityUtils;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RoomController implements RoomsApi {
+    private final Executor controllerExecutor;
+    private final RoomService roomService;
+
+    public RoomController(Executor controllerExecutor, RoomService roomService) {
+        this.controllerExecutor = controllerExecutor;
+        this.roomService = roomService;
+    }
+
+    @Override
+    public CompletableFuture<ResponseEntity<RoomDTO>> getRoom(Long id) {
+        return supplyAsync(() -> roomService.getRoom(id), controllerExecutor).thenApply(ResponseEntityUtils::ok);
+    }
+}
